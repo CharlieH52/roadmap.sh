711ed26ae29c07bff07a5fce9e2c7fce9f376366
CharlieH52 <carlos.gomez.h52@gmail.com>
2025-05-01 03:38:32 -0600

    [FEAT] catch_task and update_task functions added to save the task incoming changes

--- Backend/001-Task-Tracker/taskManager.py
+++ Backend/001-Task-Tracker/taskManager.py
@@ -1,5 +1,4 @@
 from taskClass import Task
-
 from datetime import date
 import os
 import json
@@ -11,7 +10,8 @@ fileTasks = os.path.join(folderTask,'taskLib.json')
 
 class TaskManager:
     def __init__(self):
-        pass
+        self.folder_task()
+        self.task_lib_checker()
 
     def folder_task(self):
         if not os.path.exists(folderTask):
@@ -33,26 +33,52 @@ class TaskManager:
             task_list = json.load(file)
         return task_list
 
+    def write_file(self, data_dict):
+        with open(fileTasks, "w") as file:
+            json.dump(data_dict, file, indent=4)
+
     def get_today_date(self):
         today = date.today()
         return today
 
+    def generate_id(self):
+        taskList = self.load_tasks()
+        if not taskList:
+            return 1
+        max_id = max(task['id'] for task in taskList)
+        return max_id + 1
+
     def add_task(self, fields):
         newTask = self.load_tasks()
         newTask.append(fields)
-        with open(fileTasks, "w") as file:
-            json.dump(newTask, file, indent=4)
+        self.write_file(newTask)
 
-    def delete_task(self):
-        pass
+    def delete_task(self, id):
+        list = self.load_tasks()
+        for i in list:
+            if i['id'] == id:
+                list.pop(list.index(i))
+                self.write_file(list)
 
-    def update_task(self):
-        pass
+    def update_task(self, id, data):
+        taskList = self.load_tasks()
+        for task in taskList:
+            if task['id'] == int(id):
+                for key, value in task.items():
+                    if key in data:
+                        task[key] = data[f'{key}']
+        self.write_file(taskList)
+
+    def catch_task(self, id):
+        taskList = self.load_tasks()
+        for task in taskList:
+            if task['id'] == int(id):
+                return task
 
     def list_tasks(self):
         tasks = self.load_tasks()
         if len(tasks) == 0:
-            print('No hay tareas para mostrar') 
+            print('Task list is empty, add a new task!') 
         
         if len(tasks) > 0:
             for i in tasks:
