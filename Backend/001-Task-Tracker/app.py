210445388c164be169509f85cfd2fe74690d6d04
CharlieH52 <carlos.gomez.h52@gmail.com>
2025-05-01 03:41:37 -0600

    [FEAT] Task update option was added with other subsystem for better edits

--- Backend/001-Task-Tracker/app.py
+++ Backend/001-Task-Tracker/app.py
@@ -7,9 +7,6 @@ def clear():
 
 tm = TaskManager()
 
-tm.folder_task()
-tm.task_lib_checker()
-
 while True:
     print('Task manager')
     print('What do you want to do?')
@@ -20,26 +17,96 @@ while True:
     print('Press the number of the option or e to exit')
     value = input()
 
-    if int(value) == 1:
+    if value == '1':
         clear()
-        date = f'{tm.get_today_date()}'
+        id = tm.generate_id()
+        addDate = f'{tm.get_today_date()}'
         print('Type the name of your task:')
         title = input()
         print('The default state of your task is "Unstarted"')
         status = 'Unstarted'
         print('Type the description for your task:')
-        message = input()
-        taskObj = Task(title, date, status, message)
+        description = input()
+        taskObj = Task(id=id, title=title, addDate=addDate, status=status, description=description)
         task = taskObj.to_dict()
         tm.add_task(task)
 
-    if int(value) == 2:
-        pass
+    if value == '2':
+        clear()
+        print('Type the id for update the task:')
+        idTask = input()
+        newDate = f'{tm.get_today_date()}'
+        data = tm.catch_task(idTask)
+        while True:
+            clear()
+            print('This is the information of your task:\n',
+                  f'ID: {data["id"]}\n',
+                  f'Add date: {data["addDate"]}\n',
+                  f'Title: {data["title"]}, Status: {data["status"]}\n',
+                  f'Description: {data["description"]}\n'
+                  f'Last update: {data["upDate"]}\n'
+                  )
+            print('Chose the option to edit:\n',
+                  '1. Rename title\n',
+                  '2. Change status\n',
+                  '3. Edit description\n',
+                  'S. Save the changes\n',
+                  'Type e or E to cancel'
+                  )
+            option = input()
 
-    if int(value) == 3:
-        pass
+            if option == '1':
+                print('Type the new title for your task:')
+                title = input()
+                data['title'] = title
 
-    if int(value) == 4:
+            if option == '2':
+                while True:
+                    clear()
+                    print('Type the number of the option.\n',
+                        'Change the status for your task:\n',
+                        '1. ToDo\n',
+                        '2. Started\n',
+                        '3. Done\n'
+                        'e or E to exit.'
+                        )
+                    status = input()
+                    if status == '1':
+                        data['status'] = 'ToDo'
+                        break
+                    
+                    if status == '2':
+                        data['status'] = 'Started'
+                        break
+
+                    if status == '3':
+                        data['status'] = 'Done'
+                        break
+
+                    if status == 'e' or status == 'E':
+                        break
+
+            if option == '3':
+                print('Type a new description for your task:')
+                description = input()
+                data['description'] = description
+
+            if option == 's' or option == 'S':
+                data['upDate'] = newDate
+                tm.update_task(idTask, data)
+                break
+            
+            if option == 'e' or option == 'E':
+                break
+
+    if value == '3':
+        clear()
+        print('Type the ID of the task to delete.')
+        taskID = int(input())
+        tm.delete_task(taskID)
+        print('Task has been deleted.')
+
+    if value == '4':
         clear()
         tm.list_tasks()
 
